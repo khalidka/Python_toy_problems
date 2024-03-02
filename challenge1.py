f788cdf544d5e65332817a191819dbe2e8ad1c66
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-02 17:57:13 +0300

    feat: inialize a function

--- challenge1.py
+++ challenge1.py
@@ -6,3 +6,6 @@
 # function solution(A);
 
 # that, given an array A of N integers, returns the minimum number of moves needed to end up with exactly 10 bricks in every box. If this is not possible, the function should return −1.
+
+def solution(A):
+    pass
