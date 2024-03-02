13b8de892a4b034345c0ade8028264f2b2560138
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-02 17:09:45 +0300

    first commit

---
+++ challenge1.py
@@ -0,0 +1,8 @@
+# There are N boxes (numbered from 0 to N−1) arranged in a row. The K-th box contains A[K] bricks. In one move you can take one brick from some box and move it to a box next to it (on the left or on the right). What is the minimum number of moves needed to end up with exactly 10 bricks in every box?
+
+
+# Write a function:
+
+# function solution(A);
+
+# that, given an array A of N integers, returns the minimum number of moves needed to end up with exactly 10 bricks in every box. If this is not possible, the function should return −1.
