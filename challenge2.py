2567a94e7bb1bf190856fc5abcb084c876740c13
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-03 18:09:53 +0300

    feat: set max sum to -1

--- challenge2.py
+++ challenge2.py
@@ -5,7 +5,8 @@
 # that, given an array A consisting of N integers, returns the maximum sum of two numbers whose digits add up to an equal sum. If there are no two numbers whose digits have an equal sum, the function should return -1.
 
 def soluion(A):
-    
+     # Initialize the maximum sum to -1
+    max_sum = -1  
 
 
    
