0ed154e3d28e022ac092d1a5ea65c6ce7f185951
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-03 18:07:57 +0300

    feat: set the function (solution(A))

---
+++ challenge2.py
@@ -0,0 +1,13 @@
+# Write a function:
+
+# function solution(A);
+
+# that, given an array A consisting of N integers, returns the maximum sum of two numbers whose digits add up to an equal sum. If there are no two numbers whose digits have an equal sum, the function should return -1.
+
+def soluion(A):
+    
+
+
+   
+
+    
