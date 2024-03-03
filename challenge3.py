69b82a78e217d40fe6c1fb543a9e1e9a5c860e1c
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-03 20:01:27 +0300

    feat: generate the lowercase letters of tthe repeated sequence

--- challenge3.py
+++ challenge3.py
@@ -1,4 +1,8 @@
 # Write a function solution that, given an integer N, returns a string of length N containing as many different lower-case letters ('a'-'z') as possible, in which each letter occurs an equal number of times.
 
 def solution(N):
-    
+    # Determine the number of times to repeat the sequence
+    num_repeats = (N + 25) // 26      # 26 represent the letters in the alphabet
+
+    # Generate the repeated sequence of lowercase letters
+    repeated_letters = ''.join(chr(ord('a') + i % 26) for i in range(num_repeats * 26))
