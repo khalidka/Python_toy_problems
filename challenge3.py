f7f32c46c85a38800b4d7fdb8e180365c738d9f2
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-03 19:55:48 +0300

    feat: set solution(A) function

---
+++ challenge3.py
@@ -0,0 +1,4 @@
+# Write a function solution that, given an integer N, returns a string of length N containing as many different lower-case letters ('a'-'z') as possible, in which each letter occurs an equal number of times.
+
+def solution(N):
+    
